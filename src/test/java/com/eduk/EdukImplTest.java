56f0aa243fb4ee566211f8f1b9b17a1f73b6b931
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-07-20 22:55:41 -0500

    Initial tests commit

---
+++ src/test/java/com/eduk/EdukImplTest.java
@@ -0,0 +1,9 @@
+package com.eduk;
+
+public class EdukImplTest {
+//    @Test(invocationcount = 100, threadPoolSize = 5)
+//    public void testTranslateDummy() throws Exception {
+//        int response = CalculatorTester.test("1-1+4/2",2);
+//        Assert.assertEquals(response, 1);
+//    }
+}
