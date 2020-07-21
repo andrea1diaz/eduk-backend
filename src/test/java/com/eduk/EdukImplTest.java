3f5c27bd09a7b20972cdf1294c775f28bcc46915
Diego Linares <33405883+DiegoELT@users.noreply.github.com>
2020-07-20 23:40:23 -0500

    First example of a working test.

--- src/test/java/com/eduk/EdukImplTest.java
+++ src/test/java/com/eduk/EdukImplTest.java
@@ -1,9 +1,19 @@
 package com.eduk;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.eduk.model.*;
+import com.eduk.repository.*;
+
 public class EdukImplTest {
-//    @Test(invocationcount = 100, threadPoolSize = 5)
-//    public void testTranslateDummy() throws Exception {
-//        int response = CalculatorTester.test("1-1+4/2",2);
-//        Assert.assertEquals(response, 1);
-//    }
+    @Autowired
+    UserRepository userRepository;
+
+    @Test(invocationCount = 100, threadPoolSize = 5)
+    public void testTranslateDummy() throws Exception {
+      // insert into users(id, email, first_name, last_name, password) values(3,"dlinares@atumplaya.com","Diego","Linares","$2a$10$/KnfV1UjrZxuCDQFMlkOOO924NQgxv6.ntQfdcudwT3QjcgFUvnNO"). Or register ir manually (RECOMMENDED).
+      User testUser = new User("Diego", "Linares", "dlinares@atumplaya.com", "diego123@");
+      Assert.assertEquals(testUser.getFirstName(), userRepository.findByEmail("dlinares@atumplaya.com").get().getFirstName()); 
+    }
 }
